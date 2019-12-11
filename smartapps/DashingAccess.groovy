65c5e31c48be182d77797cacc14e718402afec0b
Johnny Young <johnny@MacBook-Air.T-mobile.com>
2019-12-10 21:11:41 -0500

    first commit, started ST apps

--- smartapps/DashingAccess.groovy
+++ smartapps/DashingAccess.groovy
@@ -1,28 +1,27 @@
 /**
- *  Dashing Access
+ *  Briarcliffe Tavern Dashboard
  *
- *  Copyright 2014 florianz
+ *  Copyright 2019 Johnny Young
  *
- *  Author: florianz
- *  Contributor: bmmiller, Dianoga, mattjfrank, ronnycarr
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
  *
  */
-
-
-//
-// Definition
-//
 definition(
-    name: "Dashing Access",
-    namespace: "florianz",
-    author: "florianz",
-    description: "API access for Dashing dashboards.",
-    category: "Convenience",
+    name: "Briarcliffe Tavern Dashboard",
+    namespace: "briarcliffe-tavern-dash",
+    author: "Johnny Young",
+    description: "Briarcliffe Tavern Dashboard",
+    category: "My Apps",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    oauth: true) {
-}
-
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
 
 //
 // Preferences
